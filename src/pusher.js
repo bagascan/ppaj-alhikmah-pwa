9e162e5fc4c3086450e64e73efce58cff9d65181
bagascan <bagascndr@gmail.com>
2025-10-28 08:27:25 +0700

    penyesuaian variabel

--- src/pusher.js
+++ src/pusher.js
@@ -1,7 +1,7 @@
 import Pusher from 'pusher-js';
 
-const pusherInstance = new Pusher(process.env.PUSHER_KEY, {
-    cluster: process.env.PUSHER_CLUSTER,
+const pusherInstance = new Pusher(process.env.REACT_APP_PUSHER_KEY, {
+    cluster: process.env.REACT_APP_PUSHER_CLUSTER,
     authEndpoint: '/api/auth/pusher',
     auth: {
         headers: {
